13f3fa5b73af0529ac35e7e7a3047de7638b956e
valentine974 <valentine.bathany@hotmail.fr>
2022-12-08 08:39:20 +0100

    solved iterations

--- index.js
+++ index.js
@@ -1,17 +1,88 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    if(this.items.length){
+      this.items.sort((a,b)=> a-b) }
+    this.length = this.items.length;
+    
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items.length-1<pos){
+      throw new Error('OutOfBounds');
+    } 
+    else { 
+      return this.items[pos]
 
-  max() {}
+  }}
 
-  min() {}
+  max() {
 
-  sum() {}
+    if(!this.items.length){
+      throw new Error('EmptySortedList'); }
+      else {
+        return this.items[this.length-1]
+      }
+  
+  }
 
-  avg() {}
+  min() {
+
+    if(!this.items.length){
+      throw new Error('EmptySortedList'); }
+      else {
+        return this.items[0]
+      }
+  }
+
+  // sum() { 
+  //   if(!this.items.length){
+  //     return 0 }
+  //     else {
+  //       let sumValue = this.items.reduce((firstValue, currentValue)=> {firstValue + currentValue},0);
+  //       return sumValue
+  //     }
+  // }
+
+  sum() { 
+    if(!this.items.length){
+      return 0 }
+      else { 
+        let sum = 0;
+        for (let i=0; i<this.items.length; i++){
+          sum += this.items[i]
+
+        }
+        return sum
+        
+           
+      }
+  }
+
+  avg() {
+    if(!this.items.length){ throw new Error('EmptySortedList'); }
+      else {
+        let avg = this.sum()/this.length
+    return avg
+      }
+    
+  }
 }
 
 module.exports = SortedList;
+
+const list = new SortedList();
+list.add(2);
+list.add(5);
+list.add(7);
+list.add(6);
+list.get(2);
+list.max();
+list.min();
+list.sum();
+list.avg()
